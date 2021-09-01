941b92b2c0d4bb3e166c163a8337bdc7847f7619
Junior Martins <amjr.box@gmail.com>
2021-09-01 16:56:03 -0300

    Aula 09 - adição de eventos

--- aulas/aula05a08/my-app/src/app.js
+++ aulas/aula05a08/my-app/src/app.js
@@ -1,31 +1,9 @@
-const menu = [
-  {
-    id: 1,
-    text: 'Home',
-    link: '/',
-  },
-  {
-    id: 2,
-    text: 'Sobre',
-    link: '/sobre',
-  },
-  {
-    id: 3,
-    text: 'Contato',
-    link: '/contato',
-  },
-]
-
 function App() {
-  return (
-    <ul>
-      {menu.map((item, index) => (
-        <li key={index}>
-          <a href={item.link}>{item.text}</a>
-        </li>
-      ))}
-    </ul>
-  )
+  const handleClick = () => {
+    console.log('Clicado!')
+  }
+
+  return <button onClick={handleClick}>Clique aqui</button>
 }
 
 export default App
