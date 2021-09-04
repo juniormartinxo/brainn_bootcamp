04c8b1c9c943496bbb5c7ccceaab94479caec217
Junior Martins <amjr.box@gmail.com>
2021-09-04 19:21:36 -0300

    Aula 16 - uso do useEffect

--- aulas/aula05a08/my-app/src/app.js
+++ aulas/aula05a08/my-app/src/app.js
@@ -1,27 +1,21 @@
+import { useEffect } from 'react'
+
 const url = 'https://ws.apicep.com/cep.json?code=[CEP]'
 
 function App() {
-  function handleClick() {
-    fetch(url.replace('[CEP]', document.getElementById('cep').value))
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        document.getElementById('result').innerHTML = `
-                    <p>${data.address}</p>
-                    <p>${data.district}</p>
-                    <p>${data.city}</p>
-                    <p>${data.state}</p>
-                `
-      })
-  }
+  useEffect(() => {
+    async function getCep() {
+      const response = await fetch(url.replace('[CEP]', '06233-030'))
+      const data = await response.json()
+      console.log(data)
+    }
+    getCep()
+  })
 
   return (
-    <div>
-      <h1>CEP</h1>
-      <input type='text' id='cep' />
-      <button onClick={handleClick}>Buscar</button>
-      <div id='result'></div>
-    </div>
+    <>
+      <button>Buscar CEP</button>
+    </>
   )
 }
 
