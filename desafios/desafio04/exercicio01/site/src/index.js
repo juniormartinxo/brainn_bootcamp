b53e5710158c73d9ece6005e1d5a70f2835b6ddc
Junior Martins <amjr.box@gmail.com>
2021-09-09 16:11:37 -0300

    Desafio 04 :: Site - Exclusão dos arquivos CSS

--- desafios/desafio04/exercicio01/site/src/index.js
+++ desafios/desafio04/exercicio01/site/src/index.js
@@ -1,10 +1,4 @@
 import ReactDOM from 'react-dom'
 import App from './app'
-/*
-import './styles/reset.css'
-import './styles/style.css'
-import './styles/typography.css'
-import './styles/buttons.css'
-*/
 
 ReactDOM.render(<App />, document.getElementById('root'))
